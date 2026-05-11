bb89251618de94722b5b916223fde088e09aebd4
Sentience Engine <dev@sentience.engine>
2026-05-11 21:40:45 +0200

    Make icons white

--- src/components/finance/TransactionList.tsx
+++ src/components/finance/TransactionList.tsx
@@ -126,7 +126,7 @@ export function TransactionList({ accounts, initialTransactions = [], onTransact
       <div className="space-y-3">
         <div className="flex gap-2">
           <div className="relative flex-1">
-            <Search size={16} className="absolute left-3 top-1/2 -translate-y-1/2" style={{ color: '#A78BFA' }} />
+            <Search size={16} className="absolute left-3 top-1/2 -translate-y-1/2 text-white" />
             <input
               type="text"
               placeholder="Search transactions..."
