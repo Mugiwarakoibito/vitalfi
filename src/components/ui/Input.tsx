08012cbcaa939c1bf6aa49b2550a38a0685a879a
Sentience Engine <dev@sentience.engine>
2026-05-11 21:26:37 +0200

    Fix icon visibility in input

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -19,7 +19,7 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
         )}
         <div className="relative">
           {icon && (
-            <div className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-500 pointer-events-none">
+            <div className="absolute left-3 top-1/2 -translate-y-1/2 text-muted pointer-events-none">
               {icon}
             </div>
           )}
