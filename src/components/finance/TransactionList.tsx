ceae172d9c70115bc92d15e3f656bf4fd23e50ba
Sentience Engine <dev@sentience.engine>
2026-05-11 22:08:29 +0200

    Add elegant glow to search icon

--- src/components/finance/TransactionList.tsx
+++ src/components/finance/TransactionList.tsx
@@ -126,7 +126,7 @@ export function TransactionList({ accounts, initialTransactions = [], onTransact
       <div className="space-y-3">
         <div className="flex gap-2">
           <div className="relative flex-1">
-            <Search size={18} className="absolute left-3 top-1/2 -translate-y-1/2 text-white" style={{ zIndex: 50 }} />
+            <Search size={18} className="absolute left-3 top-1/2 -translate-y-1/2 text-white/70" style={{ zIndex: 50, filter: 'drop-shadow(0 0 6px rgba(255,255,255,0.3))' }} />
             <input
               type="text"
               placeholder="Search transactions..."
