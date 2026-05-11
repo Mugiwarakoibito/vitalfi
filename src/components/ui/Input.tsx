5c9cb165e20423149684c1af0b2433a167e4dc0a
Sentience Engine <dev@sentience.engine>
2026-05-11 21:29:02 +0200

    Make input icon more visible

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -19,7 +19,7 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
         )}
         <div className="relative">
           {icon && (
-            <div className="absolute left-3 top-1/2 -translate-y-1/2 text-muted pointer-events-none">
+            <div className="absolute left-3 top-1/2 -translate-y-1/2 text-primary-light pointer-events-none z-10">
               {icon}
             </div>
           )}
